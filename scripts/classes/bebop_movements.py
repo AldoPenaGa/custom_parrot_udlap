752597641bba257bfade24294c1633586eb02fbc
AldoPenaGa <aldo.penaga@udlap.mx>
2024-10-12 13:41:01 -0600

    Edited bebop_movements.py

--- scripts/classes/bebop_movements.py
+++ scripts/classes/bebop_movements.py
@@ -9,16 +9,15 @@ class BebopMovements:
         self.pub_takeoff = pub_takeoff
         self.pub_land = pub_land
         self.twist = Twist()
-        self.reset_twist
+        self.reset_twist()
 
     def initial_takeoff(self):
         print('Taking off...')
         self.pub_takeoff.publish(Empty())
 
-        # Setting level
-        for i in range(3):
-            self.twist.linear.z = 1
-            self.pub.publish(self.twist)
+        # Setting level, you can just publish the twist once
+        self.twist.linear.z = 1
+        self.pub.publish(self.twist)
 
     def landing(self):
         self.reset_twist()
@@ -29,51 +28,43 @@ class BebopMovements:
 
     def forward(self):
         print('Going forward...')
-        for _ in range(2):
-            self.twist.linear.x = 1
-            self.pub.publish(self.twist)
+        self.twist.linear.x = 1
+        self.pub.publish(self.twist)
 
     def left(self):
         print('Going left...')
-        for _ in range(1):
-            self.twist.linear.y = 1
-            self.pub.publish(self.twist)
+        self.twist.linear.y = 1
+        self.pub.publish(self.twist)
 
     def right(self):
         print('Going right...')
-        for _ in range(1):
-            self.twist.linear.y = -1
-            self.pub.publish(self.twist)
+        self.twist.linear.y = -1
+        self.pub.publish(self.twist)
 
     def backwards(self):
-        print('Going backwards')
-        for _ in range(2):
-            self.twist.linear.x = -1
-            self.pub.publish(self.twist)
+        print('Going backwards...')
+        self.twist.linear.x = -1
+        self.pub.publish(self.twist)
 
     def down(self):
         print('Going down...')
-        for _ in range(2):
-            self.twist.linear.z = -1
-            self.pub.publish(self.twist)
+        self.twist.linear.z = -1
+        self.pub.publish(self.twist)
 
     def up(self):
         print('Going up...')
-        for _ in range(2):
-            self.twist.linear.z = 1
-            self.pub.publish(self.twist)
+        self.twist.linear.z = 1
+        self.pub.publish(self.twist)
 
     def turn_left(self):
         print('Turning left...')
-        for _ in range(2):
-            self.twist.angular.z = 1
-            self.pub.publish(self.twist)
+        self.twist.angular.z = 1
+        self.pub.publish(self.twist)
 
     def turn_right(self):
         print('Turning right...')
-        for _ in range(2):
-            self.twist.angular.z = -1
-            self.pub.publish(self.twist)
+        self.twist.angular.z = -1
+        self.pub.publish(self.twist)
 
     def reset_twist(self):
         self.twist.linear.x = 0
